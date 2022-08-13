ea200d9dc16d0108ee4bf8c0222170b53aefa8c3
Naasir Bush <naasirbush@icloud.com>
2022-08-13 06:50:57 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import ScotchInfoBar from "./ScotchInfoBar";
+import "./styles.css";
+
+function App() {
+  const [books, setBooks] = useState(null);
+  const apiURL = "https://www.anapioficeandfire.com/api/books?pageSize=30";
+
+  function fetchBooks() {
+    fetch(apiURL)
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        setBooks(data);
+      });
+  }
+
+  return (
+    <div className="App">
+      <h1>Game of Thrones Books</h1>
+      <h2>Fetch a list from an API and display it</h2>
+
+      {/* Fetch data from API */}
+      <div>
+        <button className="fetch-button" onClick={fetchBooks}>
+          Fetch Data
+        </button>
+        <br />
+      </div>
+
+      {/* Display data from API */}
+
+      {/* Use JSX below for each book */}
+      <div className="books">
+        {books &&
+          books.map((book, index) => {
+            const authors = book.authors.join(", ");
+            const cleanedDate = new Date(book.released).toDateString();
+
+            return (
+              <div className="book" key={index}>
+                <h3>Book {index + 1}</h3>
+                <h2>{book.name}</h2>
+
+                <div className="details">
+                  <p>👨: {authors}</p>
+                  <p>📖: {book.numberOfPages}</p>
+                  <p>🏘️: {book.country}</p>
+                  <p>⏰: {cleanedDate}</p>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+
+      <ScotchInfoBar seriesNumber="7" />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
